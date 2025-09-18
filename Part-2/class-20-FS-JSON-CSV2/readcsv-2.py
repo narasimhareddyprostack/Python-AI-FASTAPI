edf4f614fe7c201be4769724c3ecaddb15876969
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-09-18 13:05:52 +0530

    ts

---
+++ Part-2/class-20-FS-JSON-CSV2/readcsv-2.py
@@ -0,0 +1,6 @@
+import csv 
+fp=open('emp.csv','r')
+emp_data=csv.reader(fp)
+
+for emp in list(emp_data)[1:]:
+    print(emp)
---
+++ Part-2/class-20-FS-JSON-CSV2/readcsv.py
@@ -0,0 +1,8 @@
+import csv 
+fp=open('emp.csv','r')
+emp_reader=csv.reader(fp)
+employees=list(emp_reader)
+print(employees)
+
+for emp in employees[1:]:
+    print(emp[1])
