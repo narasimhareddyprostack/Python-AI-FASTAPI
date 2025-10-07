25cfd5b1ccfdd59907c0eb649a121f95316c7b53
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-10-07 13:16:55 +0530

    python oop

---
+++ Part-3/class-30-OOP-4/one.py
@@ -0,0 +1,11 @@
+class Order:
+    ''' created by Narasimha '''
+    pass 
+
+o1=Order(11,'MP1',35)
+o1=Order(12,'MP2',40)
+o1=Order(13,'MP3',45)
+
+print(o1.__dict__)
+print(o2.__dict__)
+print(o3.__dict__)
