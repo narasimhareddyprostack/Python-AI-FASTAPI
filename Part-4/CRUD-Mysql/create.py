610c136d0c24834a6d180856eb0f0dc6d608d2a8
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-10-08 13:26:00 +0530

    ts

---
+++ Part-4/CRUD-Mysql/create.py
@@ -0,0 +1,20 @@
+import mysql.connector
+dbcon=mysql.connector.connect(host="localhost",
+                              user='root',
+                              password='root',
+                              database='onedb')
+print(dbcon)
+cursor=dbcon.cursor()
+print(type(cursor))
+sql_st='''
+        create table employees(
+        eid int,
+        ename varchar(32),
+        esal float
+        );
+       '''
+cursor.execute(sql_st)
+dbcon.commit()
+print("Table Created sucessfully!")
+cursor.close()
+dbcon.close()
